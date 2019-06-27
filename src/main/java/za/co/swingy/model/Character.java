217dbc96dd280d7707cf0cf4836aabfd454e3ace
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-06-27 11:53:25 +0200

    Setting up project

---
+++ src/main/java/za/co/swingy/model/Character.java
@@ -0,0 +1,10 @@
+package za.co.swingy.model;
+
+public class Character {
+//	private String		name;
+	private int			level;
+	private int			attack;
+	private int			defence;
+	private int			hitPoints;
+
+}
---
+++ src/main/java/za/co/swingy/model/Weapon.java
@@ -0,0 +1,11 @@
+package za.co.swingy.model;
+
+public class Weapon {
+	private int			attackIncrease;
+	private String		weaponName;
+	private String[]	weapons = {"Sword", "Axe", "Great Sword", "Crossbow", "Javelin"};
+
+	public void			generateWeapon(int level) {
+		
+	}
+}
