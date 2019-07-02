f19f096319d9333156b947dd4d431ba1e75dc08a
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-02 14:03:34 +0200

    Setting up hero class

--- src/main/java/za/co/swingy/model/characters/Hero.java
+++ src/main/java/za/co/swingy/model/characters/Hero.java
@@ -3,6 +3,7 @@ package za.co.swingy.model.characters;
 import lombok.Getter;
 import lombok.Setter;
 import za.co.swingy.model.items.Armor;
+import za.co.swingy.model.items.Inventory;
 import za.co.swingy.model.items.Weapon;
 
 import javax.validation.constraints.NotNull;
@@ -16,4 +17,37 @@ public class Hero extends Character {
 	private Armor		equippedArmor;
 	@NotNull
 	private  int		experience;
+	@NotNull
+	private Inventory	inventory;
+
+	public				Hero(Builder builder) {
+		this.equippedWeapon = builder.equippedWeapon;
+		this.equippedArmor = builder.equippedArmor;
+		this.name = builder.name;
+	}
+	// Builder
+	public static class Builder {
+		private Weapon		equippedWeapon;
+		private Armor		equippedArmor;
+		private  int		experience;
+		private Inventory	inventory;
+
+		public static Builder newInstance() {
+			return new Builder();
+		}
+
+		private Builder() {}
+
+		public Builder setLevel(int level) {
+			this.level = level;
+
+			return this;
+		}
+
+
+		public Weapon 				build() {
+			return new Hero(this);
+		}
+
+	}
 }
