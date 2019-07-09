09b4ba3138e16bc53182a299fa5be1c5ddaccda4
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-09 13:28:23 +0200

    Setting up to load

--- src/main/java/za/co/swingy/controller/CharacterController.java
+++ src/main/java/za/co/swingy/controller/CharacterController.java
@@ -7,6 +7,7 @@ import za.co.swingy.model.characters.Hero;
 import za.co.swingy.view.CreateHero;
 
 import javax.validation.constraints.NotNull;
+import java.io.*;
 
 @Getter
 @Setter
@@ -25,11 +26,59 @@ public class CharacterController {
 			return this;
 		}
 	}
-		public void			createNewHero() {
+
+	public void			createNewHero() {
 		String name = this.createHeroView.promptName();
 		String type = this.createHeroView.promptType();
 		this.hero = Hero.builder().classType(type).name(name).inventory().build();
 		this.hero.starterHero();
 		this.createHeroView.printHeroStatus(this.hero);
+	}
+
+	public void			loadHero() {
+		//Get File
+		//List available heroes
+		//Take input
+		//Assign Hero
+	}
+
+	public int				readFile(String input) {
+		int numberOfRuns = 0;
+		try {
+			File file = new File(input);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String st;
+
+			int     line = 0;
+			// Read file
+			try {
+				while ((st = br.readLine()) != null) {
+					try {
+						if (line == 0) {
+							if (isNumeric(st)) {
+								numberOfRuns = Integer.parseInt(st);
+							} else {
+								throw new InvalidFileException((char) 27 + "[31m" + "ERROR: " + (char) 27 + "[37m" + "Invalid first line");
+							}
+						} else {
+							//Create and register aircraft
+							String details[] = st.split(" ");
+							if (this.interpretLine(details) != 1) {
+								return (-1);
+							}
+						}
+					} catch (InvalidFileException e) {
+						e.printStackTrace();
+					}
+					line++;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}  catch ( FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return numberOfRuns;
 	}
 }
