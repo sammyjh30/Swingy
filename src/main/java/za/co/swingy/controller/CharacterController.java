b9109cfd23b8fca77ca3f555b83a3a63e4ca2bd4
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-10 13:01:53 +0200

    Setting up loads

--- src/main/java/za/co/swingy/controller/CharacterController.java
+++ src/main/java/za/co/swingy/controller/CharacterController.java
@@ -4,10 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import za.co.swingy.model.characters.Hero;
+import za.co.swingy.model.items.Armor;
+import za.co.swingy.model.items.Helm;
+import za.co.swingy.model.items.Weapon;
 import za.co.swingy.view.CreateHero;
 
 import javax.validation.constraints.NotNull;
 import java.io.*;
+import java.util.ArrayList;
 
 @Getter
 @Setter
@@ -37,12 +41,22 @@ public class CharacterController {
 
 	public void			loadHero() {
 		//Get File
+		ArrayList<Hero> saves = readSaves();
 		//List available heroes
 		//Take input
 		//Assign Hero
 	}
 
-	public void				readSaves() {
+	public static boolean 	isNumeric(String str) {
+		try {
+			double d = Double.parseDouble(str);
+		} catch(NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	public ArrayList<Hero>				readSaves() {
 //		int numberOfRuns = 0;
 		String st;
 		try {
@@ -54,48 +68,83 @@ public class CharacterController {
 //				writer.close();
 				//To load
 				BufferedReader br = new BufferedReader(new FileReader(file));
+				ArrayList<Hero> heroSaves = new ArrayList<Hero>();
+				Hero heroToAdd = null;
+				Weapon weapon;
+				Armor armor;
+				Helm helm;
 				try {
+					int save = 0;
+					String line[];
 					while ((st = br.readLine()) != null) {
-						//1|Name|Type|Level|
+						//1|Name|Type|Level|Attack|Defence|HitPoints|MaxHitPoints|Experience|
+						//1|Inventory|usedSlots|MaxSlots|equippedWeaponIndex|equippedArmorIndex|equippedHelmIndex
+						//1|Weapons|Index|Name|Level|AttackIncrease
+						//1|Armors|Index|Name|Level|DefenceIncrease
+						//1|Helms|Index|Name|Level|hitpointIncrease
+						line = st.split("|");
+						if (isNumeric(line[0])){
+							if (Integer.parseInt(line[0]) > save) {
+								if (heroToAdd != null) {
+									heroSaves.add(heroToAdd);
+								}
+								save = Integer.parseInt(line[0]);
+								//Create new character
+								heroToAdd = Hero.builder().name(line[1]).classType(line[2]).inventory().build();
+								heroToAdd.setLevel(Integer.parseInt(line[3]));
+								heroToAdd.setAttack(Integer.parseInt(line[4]));
+								heroToAdd.setDefence(Integer.parseInt(line[5]));
+								heroToAdd.setHitPoints(Integer.parseInt(line[6]));
+								heroToAdd.setMaxHitPoints(Integer.parseInt(line[7]));
+								heroToAdd.setExperience(Integer.parseInt(line[8]));
+							} else if (Integer.parseInt(line[0]) == save && line[1] == "Inventory") {
+								//add the inventory to the hero
+								heroToAdd.getInventory().setUsedSlots(Integer.parseInt(line[2]));
+								heroToAdd.getInventory().setMaxSlots(Integer.parseInt(line[3]));
+								heroToAdd.getInventory().setEquippedWeaponIndex(Integer.parseInt(line[4]));
+								heroToAdd.getInventory().setEquippedArmorIndex(Integer.parseInt(line[5]));
+								heroToAdd.getInventory().setEquippedHelmIndex(Integer.parseInt(line[6]));
+							} else if (Integer.parseInt(line[0]) == save && line[1] == "Weapons") {
+								//Loop through and assign all weapons
+								for (int i = 2; i < line.length; i++) {
+									weapon = Weapon.builder().build();
+									weapon.setName(line[++i]);
+									weapon.setLevel(Integer.parseInt(line[++i]));
+									weapon.setAttackIncrease(Integer.parseInt(line[++i]));
+									heroToAdd.getInventory().getWeapons().add(weapon);
+								}
+								heroToAdd.setEquippedWeapon(heroToAdd.getInventory().getWeapons().get(heroToAdd.getInventory().getEquippedWeaponIndex()));
+							} else if (Integer.parseInt(line[0]) == save && line[1] == "Armors") {
+								//Loop through and assign all armors
+								for (int i = 2; i < line.length; i++) {
+									armor = Armor.builder().build();
+									armor.setName(line[++i]);
+									armor.setLevel(Integer.parseInt(line[++i]));
+									armor.setDefenceIncrease(Integer.parseInt(line[++i]));
+									heroToAdd.getInventory().getArmors().add(armor);
+								}
+								heroToAdd.setEquippedArmor(heroToAdd.getInventory().getArmors().get(heroToAdd.getInventory().getEquippedArmorIndex()));
+							} else if (Integer.parseInt(line[0]) == save && line[1] == "Helms") {
+								//Loop through and assign all helms
+								for (int i = 2; i < line.length; i++) {
+									helm = Helm.builder().build();
+									helm.setName(line[++i]);
+									helm.setLevel(Integer.parseInt(line[++i]));
+									helm.setHitPointIncrease(Integer.parseInt(line[++i]));
+									heroToAdd.getInventory().getHelms().add(helm);
+								}
+								heroToAdd.setEquippedHelm(heroToAdd.getInventory().getHelms().get(heroToAdd.getInventory().getEquippedHelmIndex()));
+							}
+						}
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				return heroSaves;
 			}
 		} catch ( FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
-			String st;
-
-			int     line = 0;
-			// Read file
-			try {
-				while ((st = br.readLine()) != null) {
-					try {
-						if (line == 0) {
-							if (isNumeric(st)) {
-								numberOfRuns = Integer.parseInt(st);
-							} else {
-								throw new InvalidFileException((char) 27 + "[31m" + "ERROR: " + (char) 27 + "[37m" + "Invalid first line");
-							}
-						} else {
-							//Create and register aircraft
-							String details[] = st.split(" ");
-							if (this.interpretLine(details) != 1) {
-								return (-1);
-							}
-						}
-					} catch (InvalidFileException e) {
-						e.printStackTrace();
-					}
-					line++;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		}
-		return numberOfRuns;
+		return null;
 	}
 }
--- src/main/java/za/co/swingy/view/LoadFile.java
+++ src/main/java/za/co/swingy/view/LoadFile.java
@@ -1,4 +1,93 @@
 package za.co.swingy.view;
 
+import za.co.swingy.model.characters.Hero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class LoadFile {
+	//Need to still save/load character position
+	public static boolean 	isNumeric(String str) {
+		try {
+			double d = Double.parseDouble(str);
+		} catch(NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+	public int			saveList(ArrayList<Hero> saves) {
+		try {
+			System.out.println("LOAD SAVE:");
+			System.out.println("_______________________________________");
+			System.out.println("| No. |		Name			|  Level  |");
+			System.out.println("|_____|_____________________|_________|");
+			for (int i = 0; i < saves.size() && i <= 100; i++) {
+				//Print index
+				if (i < 10) {
+					System.out.print("|   "+i+" |");
+				} else if (i > 9 && i < 100) {
+					System.out.print("|  "+i+" |");
+				} else if (i == 100) {
+					System.out.print("| "+i+" |");
+				}
+				//Print name
+				if (saves.get(i).getName().length() < 21) {
+					System.out.print("[%1$"+ (21 - saves.get(i).getName().length()) + "s]%n" + saves.get(i).getName() + "|");
+				} else {
+					System.out.print(saves.get(i).getName().substring(0, 20) + "|");
+				}
+				//Print level
+				if (i < 10) {
+					System.out.println("|        "+i+" |");
+				} else if (i > 9 && i < 100) {
+					System.out.println("|       "+i+" |");
+				} else if (i == 100) {
+					System.out.println("|      "+i+" |");
+				}
+			}
+
+			System.out.println("Please enter the number of the save you would like to load, or BACK to return to the menu.");
+			System.out.print(": ");
+			InputStreamReader streamReader = new InputStreamReader(System.in);
+			BufferedReader bufferedReader = new BufferedReader(streamReader);
+			String input = bufferedReader.readLine();
+			while (input == null || input.isEmpty() ||
+					(!input.equalsIgnoreCase("BACK") && (!isNumeric(input) && Integer.parseInt(input) < i))) {
+				System.out.println("Oops, that's not a valid command! Please try again!");
+				System.out.print(": ");
+				input = bufferedReader.readLine();
+			}
+			if (input.equalsIgnoreCase("BACK"))  {
+				return (-1);
+			} else {
+				return Integer
+			}
+			return name;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("LOAD SAVE:");
+	}
+
+	public String			 promptName() {
+		try {
+			System.out.println("Greetings hero! Before you begin your journey, let's set up your character!");
+			System.out.print("Please enter in your name: ");
+			InputStreamReader streamReader = new InputStreamReader(System.in);
+			BufferedReader bufferedReader = new BufferedReader(streamReader);
+			String name = bufferedReader.readLine();
+			while (name == null || name.isEmpty()) {
+				System.out.println("Oops, that's not a valid name! Please try again!");
+				System.out.print("Please enter in your name: ");
+				name = bufferedReader.readLine();
+			}
+			return name;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "Bob";
+	}
 }
