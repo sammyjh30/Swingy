e4711e17b10a490b974bf564d2aa6f26d49cbdac
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-10 18:15:32 +0200

    Setting up game steps

--- src/main/java/za/co/swingy/controller/GameController.java
+++ src/main/java/za/co/swingy/controller/GameController.java
@@ -3,4 +3,17 @@ package za.co.swingy.controller;
 
 public class GameController {
 
+	// Map
+	// Array Enemies
+	// Hero
+
+	//Menu() CREATE, LOAD or EXIT
+
+	// Place map (enemies, hero, holes)
+	// Set up starter commands (Move, inventory, save and quit)
+	// Set up interaction commands (Fight, Run)
+	// Set up victory commands
+	// Set up failure command
+	// Set up level up (Map increase, enemy increase)
+
 }
