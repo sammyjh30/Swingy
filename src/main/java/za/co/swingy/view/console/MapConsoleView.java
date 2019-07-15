2dad6e85d8a8beedf45d6e18e722e12b1d8f6b79
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-15 18:13:54 +0200

    Setting up functionality of map view

--- src/main/java/za/co/swingy/view/console/MapConsoleView.java
+++ src/main/java/za/co/swingy/view/console/MapConsoleView.java
@@ -3,36 +3,42 @@ package za.co.swingy.view.console;
 import za.co.swingy.controller.GameController;
 import za.co.swingy.view.MapView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MapConsoleView implements MapView {
 
 	public void					displayMap(char[][] map, int mapSize) {
-//		System.out.println((char)27 + "[30mYELLOW");
-//		System.out.println((char)27 + "[31m" + "ERROR MESSAGE IN RED");
-//		System.out.println((char)27 + "[32mGREEN");
-//		System.out.println((char)27 + "[33mYELLOW");
-//		System.out.println((char)27 + "[34mBLUE");
-//		System.out.println((char)27 + "[35mPURPLE");
-//		System.out.println((char)27 + "[36mLIGHTBLUE");
-//		System.out.println((char)27 + "[37mWHITE");
+		System.out.print("\n");
 		for (int l = 0; l < mapSize * 2 + 2; l++) {
 			System.out.print("_");
 		}
+		System.out.print("     N");
 		System.out.print("\n");
 		for (int i = 0; i < mapSize; i++) {
-//			System.out.format("%1$"+ (mapSize * 2) + "s", "\n");
-
 			System.out.print("| ");
 			for (int j = 0; j < mapSize; j++) {
 				if (map[i][j] == '.') {
 					System.out.print('.');
-				} else if (map[i][j] == 'O') {
-					System.out.print((char)27 + "[32mO" + (char)27 + "[37m");
 				} else if (map[i][j] == 'X') {
-					System.out.print((char)27 + "[35mX" + (char)27 + "[37m");
+					System.out.print((char)27 + "[32mX" + (char)27 + "[37m");
+				} else if (map[i][j] == 'O') {
+					System.out.print((char)27 + "[35mO" + (char)27 + "[37m");
 				}
 				System.out.print(" ");
 			}
-			System.out.print("|\n");
+			System.out.print("|");
+			if (i == 0) {
+				System.out.print("    Λ");
+			} else if (i == 1) {
+				System.out.print(" W<-|->E");
+			} else if (i == 2) {
+				System.out.print("    V");
+			} else if (i == 3) {
+				System.out.print("    S");
+			}
+			System.out.print("\n");
 		}
 		System.out.print("|");
 		for (int l = 0; l < mapSize * 2 + 1; l++) {
@@ -41,24 +47,47 @@ public class MapConsoleView implements MapView {
 		System.out.print("|\n");
 	}
 
-	public void					drawCompass() {
-		System.out.println("    ______________   ");
-		System.out.println("   /              \\  ");
-		System.out.println("  /     NORTH      \\  ");
-		System.out.println(" /        Λ         \\ ");
-		System.out.println("|         |          |");
-		System.out.println("| WEST<---O---> EAST |");
-		System.out.println("|         |          |");
-		System.out.println(" \\        V         / ");
-		System.out.println("  \\     SOUTH      /  ");
-		System.out.println("   \\______________/  ");
+	public void					showOptions() {
+		System.out.println("Your goal is to leave all the maps, or level up lo level 6.");
+		System.out.println("You can move: " + (char)27 + "[32mNORTH" + (char)27 + "[37m, " + (char)27 + "[32mSOUTH" + (char)27 + "[37m, "
+				+ (char)27 + "[32mEAST" + (char)27 + "[37m or " + (char)27 + "[32mWEST" + (char)27 + "[37m");
+		System.out.println("Or you can go to your " + (char)27 + "[32mINVENTORY" + (char)27 + "[37m or " + (char)27 + "[32mSAVE" + (char)27 + "[37m to save and exit this game.");
 	}
 
 	public void					display(GameController controller) {
 		int stage = 0;
 		while  (stage >= 0) {
 			displayMap(controller.getMap(), controller.getMapSize());
-			drawCompass();
+			try {
+				InputStreamReader streamReader = new InputStreamReader(System.in);
+				BufferedReader bufferedReader = new BufferedReader(streamReader);
+				showOptions();
+				System.out.print("Please enter a command: ");
+				String input = bufferedReader.readLine();
+				while (!input.equalsIgnoreCase("NORTH") && !input.equalsIgnoreCase("SOUTH") &&
+						!input.equalsIgnoreCase("EAST") && !input.equalsIgnoreCase("WEST") &&
+						!input.equalsIgnoreCase("INVENTORY") && !input.equalsIgnoreCase("SAVE")){
+					System.out.println("Oops, that's not a valid command! Please try again!");
+					System.out.print("Please enter a command: ");
+					input = bufferedReader.readLine();
+				}
+				if (input.equalsIgnoreCase("NORTH")) {
+					System.out.println("Move the hero north");
+				} else if (input.equalsIgnoreCase("SOUTH")) {
+					System.out.println("Move the hero south");
+				} else if (input.equalsIgnoreCase("EAST")) {
+					System.out.println("Move the hero east");
+				} else if (input.equalsIgnoreCase("WEST")) {
+					System.out.println("Move the hero west");
+				} else if (input.equalsIgnoreCase("INVENTORY")) {
+					System.out.println("Open inventory");
+				} else if (input.equalsIgnoreCase("SAVE")) {
+					System.out.println("Save the game");
+				}
+				//Clean screen
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		if (stage == -1) {
 			//Close game
