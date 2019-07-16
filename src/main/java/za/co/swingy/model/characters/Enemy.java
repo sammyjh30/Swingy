8b6aacf36a0fdfaadd4e9ba6215c7c1f2c5f8513
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-16 13:18:26 +0200

    Setting up enemies

--- src/main/java/za/co/swingy/model/characters/Enemy.java
+++ src/main/java/za/co/swingy/model/characters/Enemy.java
@@ -4,9 +4,53 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import java.util.Random;
+
 @Getter
 @Setter
 @Builder
 public class Enemy extends Character{
+	@NotNull
+	private String		enemyName;
+	@NotNull
+	private String		enemyType;
 
+	// Builder
+	public static class EnemyBuilder {
+		private String		enemyName;
+		private String		enemyType;
+
+		public EnemyBuilder			enemyName() {
+			Random rand = new Random();
+			String enemyNames[] = {"Karen","Chad","Kyle","Philip","Susan","Donald"};
+
+			String name = enemyNames[rand.nextInt(5)];
+			this.enemyName = name;
+			return this;
+		}
+
+		public EnemyBuilder			enemyType() {
+			Random rand = new Random();
+			String enemyTypes[] = {"Orc","Witch","Troll","Bandit","Spooky Scary Skeleton","Bird"};
+
+			String type = enemyTypes[rand.nextInt(5)];
+			this.enemyType = type;
+			return this;
+		}
+	}
+
+	public void				generateEnemy(Hero hero){
+		Random rand = new Random();
+		int level = (hero.getLevel() - 1) + rand.nextInt(3);
+		if (level < 0) {
+			this.setLevel(0);
+		} else {
+			this.setLevel(level);
+		}
+		this.setAttack(this.getLevel() + 3);
+		this.setDefence(this.getLevel() + 3);
+		this.setMaxHitPoints(this.getLevel() + this.getLevel() * 20);
+		this.setHitPoints(this.getLevel() + this.getLevel() * 20);
+	}
 }
