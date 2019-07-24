b35589b08c3e93c8e4aa0a980a4705ad5d1a4298
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-07-24 14:40:18 +0200

    Setting up Gui views

--- src/main/java/za/co/swingy/view/gui/CreateHeroGuiView.java
+++ src/main/java/za/co/swingy/view/gui/CreateHeroGuiView.java
@@ -3,13 +3,11 @@ package za.co.swingy.view.gui;
 import javax.swing.*;
 
 public class CreateHeroGuiView {
-	private JTextPane alrightNowItSTextPane;
-	private JTextPane pleaseEnterYourNameTextPane;
-	private JButton button1;
-	private JButton button2;
-	private JButton button3;
-	private JButton button5;
-	private JButton button6;
 	private JTextPane greetingsHeroBeforeYouTextPane;
 	private JTextField textField1;
+	private JTextArea alrightNowItSTextArea;
+	private JButton explorerButton;
+	private JButton warriorButton;
+	private JButton knightButton;
+	private JButton barbarianButton;
 }
---
+++ src/main/java/za/co/swingy/view/gui/MenuGuiView.java
@@ -0,0 +1,10 @@
+package za.co.swingy.view.gui;
+
+import javax.swing.*;
+
+public class MenuGuiView {
+	private JTextArea welcomeToSwingyWhatTextArea;
+	private JButton newButton;
+	private JButton loadButton;
+	private JButton exitButton;
+}
